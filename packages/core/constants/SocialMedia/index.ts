048cca9c122efd9148c2c86be8566206dc0a8529
Young <willy4202@naver.com>
2022-08-02 23:02:08 +0900

    초기세팅

--- packages/core/constants/SocialMedia/index.ts
+++ packages/core/constants/SocialMedia/index.ts
@@ -1,12 +1,12 @@
 // Please do not write '/' at last
 // 마지막에 '/'를 빼주세요!
 
-export const githubUrl = 'https://github.com/hyesungoh';
+export const githubUrl = 'https://github.com/willy4202';
 
-export const instagramUrl = 'https://www.instagram.com/hyes5_';
+export const instagramUrl = 'https://www.instagram.com/syoung___h';
 
-export const facebookUrl = 'https://www.facebook.com/hyesungoh414';
+export const facebookUrl = 'https://www.facebook.com/seokyeong.hwang.9';
 
-export const linkedInUrl = 'https://www.linkedin.com/in/hyesungoh414';
+export const linkedInUrl = 'https://www.linkedin.com/in/석영-황-830a51218';
 
 export const twitterUrl = '';
