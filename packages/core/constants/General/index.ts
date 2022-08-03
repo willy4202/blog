616b7100df50e92410179c6dc42b2a1ff49cc470
Young <willy4202@naver.com>
2022-08-03 10:43:21 +0900

    초기세팅

--- packages/core/constants/General/index.ts
+++ packages/core/constants/General/index.ts
@@ -5,7 +5,7 @@ export const authorName = 'Young';
 
 // Write ever you want :D, default is github
 // 직접 작성해주셔도 좋습니다. 기본적으로 깃허브 주소를 사용합니다.
-export const defaultUrl = githubUrl;
+export const defaultUrl = 'https://github.com/willy4202';
 
 // using both app's favicon
 export * as favicon from './favicon.ico';
